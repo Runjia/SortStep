4bd332ce3d7d5aed1d88ff8863c493937ca89f5d
Runjia <runjia.chen44@gmail.com>
2015-04-21 11:40:22 -0700

    Sorting

--- build-tmp/source/SortStep.java
+++ build-tmp/source/SortStep.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SortStep extends PApplet {
+
 private int numInts = 8000;
 private int largestInt = 100;
 private int [] myArray= new int[numInts];
@@ -86,4 +102,13 @@ private boolean isSorted()
     if(myArray[i] > myArray[i+1])
       return false;
   return true;
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SortStep" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
